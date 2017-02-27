4055934f7baa13c8dd1fe4adca68f6bff6aae1ef
Faldict <legadoubledyan@gmail.com>
2017-02-27 20:45:29 +0800

    git submodule

--- mavlink/include/mavlink/ASLUAV/version.h
+++ mavlink/include/mavlink/ASLUAV/version.h
@@ -1,11 +1,13 @@
 /** @file
- *	@brief MAVLink comm protocol built from ASLUAV.xml
- *	@see http://mavlink.org
+ *  @brief MAVLink comm protocol built from ASLUAV.xml
+ *  @see http://mavlink.org
  */
+#pragma once
+ 
 #ifndef MAVLINK_VERSION_H
 #define MAVLINK_VERSION_H
 
-#define MAVLINK_BUILD_DATE "Fri Nov 14 05:22:41 2014"
+#define MAVLINK_BUILD_DATE "Fri Feb 17 2017"
 #define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
 #define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 255
  
